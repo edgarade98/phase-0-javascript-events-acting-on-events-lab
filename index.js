4dbcf13b7908856c0288f83d2aaae91714ae49f0
Edgar <eddyade98@gmail.com>
2023-09-27 17:21:00 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+function moveDodgerLeft() {
+const dodger = document.getElementById('dodger'); 
+document.addEventListener("keydown", function (l) {
+    if (l.key === "ArrowLeft") {
+      moveDodgerLeft();
+      dodger.style.left = 180 - 'px';
+    }
+  });
+}
+moveDodgerLeft();
+
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger'); 
+    document.addEventListener("keydown", function (r) {
+        if (r.key === "ArrowRight") {
+          moveDodgerRight();
+          dodger.style.right = 180 + 'px';
+        }
+      });
+}
+
+moveDodgerRight();
+
